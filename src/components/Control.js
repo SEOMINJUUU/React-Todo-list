9fe0634d9d1ecadba711e335be1b3860176e270c
서민주 <minju1307@gmail.com>
2019-07-04 14:16:33 +0900

    filter css

--- src/components/Control.js
+++ src/components/Control.js
@@ -2,31 +2,59 @@ import React from 'react'
 import { FaFilter, FaTrashAlt, FaCheck } from 'react-icons/fa'
 
 class Control extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      filter: 'all',
+      hidden: true
+    }
+  }
+
   handleClick = (e) => {
     const { onFilter } = this.props
-    const filter = e.target.id
-    onFilter(filter)
+
+    onFilter(e.target.id)
+    this.setState({
+      filter: e.target.id
+    })
   }
 
   render () {
+    const { filter, hidden } = this.state
     const { onRemoveAll, onCompleted } = this.props
     const { handleClick } = this
+
     return (
       <div id='control'>
         <button
-          className='btn btn-purple'>
+          className='btn btn-purple'
+          onClick={() => this.setState(prevState => ({
+            hidden: !prevState.hidden
+          }))}>
           <FaFilter /> Filter
         </button>
-        <ul>
-          <li id='all' onClick={handleClick}> All </li>
-          <li id='todo' onClick={handleClick}> Todo </li>
-          <li id='completed' onClick={handleClick}>Completed</li>
+
+        <ul className={hidden ? 'hide' : 'show-inline-block'}>
+          <li
+            id='all'
+            className={filter === 'all' ? 'filter-on' : 'filter-off'}
+            onClick={handleClick}> All </li>
+          <li
+            id='todo'
+            className={filter === 'todo' ? 'filter-on' : 'filter-off'}
+            onClick={handleClick}> Todo </li>
+          <li
+            id='completed'
+            className={filter === 'completed' ? 'filter-on' : 'filter-off'}
+            onClick={handleClick}>Completed</li>
         </ul>
+
         <button
           className='btn btn-red'
           onClick={onRemoveAll}>
           <FaTrashAlt /> Clear All
         </button>
+
         <button
           className='btn btn-orange'
           onClick={onCompleted}>
