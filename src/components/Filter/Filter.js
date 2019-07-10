9073d57173f62f0ec9cda6f8e927f35fa5dde493
서민주 <minju1307@gmail.com>
2019-07-10 16:06:13 +0900

    delete unused vars

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,9 +2,8 @@ import React from 'react';
 import styles from './Filter.module.css';
 
 function Filter(props) {
-  const { id, onClick } = props;
+  const { id } = props;
   return (
-    // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
     <li id={id} className={styles.off}>
       {id}
     </li>
